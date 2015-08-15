94a6261c43b8a7de0be316c342baf6e808b4f6ed
d356 <d356@users.noreply.github.com>
2015-08-14 23:50:14 -0400

    .

--- yabause/src/runner/yui.c
+++ yabause/src/runner/yui.c
@@ -81,8 +81,8 @@ M68K_struct * M68KCoreList[] = {
 
 struct ConsoleColor
 {
-    char ansi[12];
-    int windows;
+   char ansi[12];
+   int windows;
 };
 
 struct ConsoleColor text_red = { "\033[22;31m",4 };
@@ -93,10 +93,10 @@ struct ConsoleColor text_light_red = { "\033[01;31m", 0xc };
 void set_color(struct ConsoleColor color)
 {
 #ifndef _MSC_VER
-    printf("%s", color.ansi);
+   printf("%s", color.ansi);
 #else
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, (WORD)color.windows);
+   HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+   SetConsoleTextAttribute(hConsole, (WORD)color.windows);
 #endif
 }
 
@@ -104,7 +104,7 @@ static const char *bios = "";
 static int emulate_bios = 0;
 
 void YuiErrorMsg(const char *error_text) {
-    fprintf(stderr, "Error: %s\n", error_text);
+   fprintf(stderr, "Error: %s\n", error_text);
 }
 
 void YuiSwapBuffers(void) {
@@ -112,211 +112,211 @@ void YuiSwapBuffers(void) {
 
 void get_test_strings(int *i, char *full_str, char *command)
 {
-    sprintf(full_str, "%s", Vdp2Ram + AUTO_TEST_OUTPUT_ADDRESS + *i);
+   sprintf(full_str, "%s", Vdp2Ram + AUTO_TEST_OUTPUT_ADDRESS + *i);
 
-    *i = *i + (int)strlen(full_str) + 1;// + 1 because of null char
+   *i = *i + (int)strlen(full_str) + 1;// + 1 because of null char
 
-    sprintf(command, "%s", Vdp2Ram + AUTO_TEST_OUTPUT_ADDRESS + *i);
+   sprintf(command, "%s", Vdp2Ram + AUTO_TEST_OUTPUT_ADDRESS + *i);
 
-    *i = *i + (int)strlen(command) + 1;
+   *i = *i + (int)strlen(command) + 1;
 }
 
 void print_result(char* test_info, char* command, int status)
 {
-    set_color(text_white);
+   set_color(text_white);
 
-    printf("%-32s ", test_info, command);
+   printf("%-32s ", test_info, command);
 
-    if (status == TEST_PASSED)
-    {
-        set_color(text_green);
-    }
-    else if (status == TEST_FAILED)
-    {
-        set_color(text_red);
-    }
-    else if (status == TEST_FAILED_EXPECTED)
-    {
-        set_color(text_light_red);
-        printf("%-20s", command);
-        set_color(text_green);
-        printf("%s", " (Not a regression)\n");
-        set_color(text_white);
-        return;
-    }
+   if (status == TEST_PASSED)
+   {
+      set_color(text_green);
+   }
+   else if (status == TEST_FAILED)
+   {
+      set_color(text_red);
+   }
+   else if (status == TEST_FAILED_EXPECTED)
+   {
+      set_color(text_light_red);
+      printf("%-20s", command);
+      set_color(text_green);
+      printf("%s", " (Not a regression)\n");
+      set_color(text_white);
+      return;
+   }
 
-    printf("%s\n", command);
+   printf("%s\n", command);
 
-    set_color(text_white);
+   set_color(text_white);
 }
 
 //add tests that currently don't work in yabause here
 char* tests_expected_to_fail[] =
 {
-    //slave sh2
-    "SCU Mask Cache Quirk",
-    "Slave VBlank",
-    //scu interrupt
-    "Timer 1 Interrupt",
-    "Pad Interrupt",
-    "Illegal DMA Interrupt",
-    //scu dma
-    "Misaligned DMA transfer",
-    "Indirect DMA transfer",
-    //scu dsp
-    "DSP Execution",
-    "MVI Imm, [d]",
-    "DSP Timing",
-    NULL
+   //slave sh2
+   "SCU Mask Cache Quirk",
+   "Slave VBlank",
+   //scu interrupt
+   "Timer 1 Interrupt",
+   "Pad Interrupt",
+   "Illegal DMA Interrupt",
+   //scu dma
+   "Misaligned DMA transfer",
+   "Indirect DMA transfer",
+   //scu dsp
+   "DSP Execution",
+   "MVI Imm, [d]",
+   "DSP Timing",
+   NULL
 };
 
 int main(int argc, char *argv[])
 {
-    yabauseinit_struct yinit = { 0 };
-    int current_test = 0;
-    int regressions = 0;
-    int tests_total = 0;
-    int tests_passed = 0;
-    int finished = 0;
-    int expected_failure_count = 0;
-    int tests_expected_to_fail_count = 0;
+   yabauseinit_struct yinit = { 0 };
+   int current_test = 0;
+   int regressions = 0;
+   int tests_total = 0;
+   int tests_passed = 0;
+   int finished = 0;
+   int expected_failure_count = 0;
+   int tests_expected_to_fail_count = 0;
 
-    printf("Running tests...\n\n");
+   printf("Running tests...\n\n");
 
-    yinit.percoretype = PERCORE_DUMMY;
-    yinit.sh2coretype = SH2CORE_INTERPRETER;
-    yinit.vidcoretype = VIDCORE_DUMMY;
-    yinit.m68kcoretype = M68KCORE_DUMMY;
-    yinit.sndcoretype = SNDCORE_DUMMY;
-    yinit.cdcoretype = CDCORE_DUMMY;
-    yinit.carttype = CART_NONE;
-    yinit.regionid = REGION_AUTODETECT;
-    yinit.biospath = emulate_bios ? NULL : bios;
-    yinit.cdpath = NULL;
-    yinit.buppath = NULL;
-    yinit.mpegpath = NULL;
-    yinit.cartpath = NULL;
-    yinit.frameskip = 0;
-    yinit.videoformattype = VIDEOFORMATTYPE_NTSC;
-    yinit.clocksync = 0;
-    yinit.basetime = 0;
-    yinit.skip_load = 1;
+   yinit.percoretype = PERCORE_DUMMY;
+   yinit.sh2coretype = SH2CORE_INTERPRETER;
+   yinit.vidcoretype = VIDCORE_DUMMY;
+   yinit.m68kcoretype = M68KCORE_DUMMY;
+   yinit.sndcoretype = SNDCORE_DUMMY;
+   yinit.cdcoretype = CDCORE_DUMMY;
+   yinit.carttype = CART_NONE;
+   yinit.regionid = REGION_AUTODETECT;
+   yinit.biospath = emulate_bios ? NULL : bios;
+   yinit.cdpath = NULL;
+   yinit.buppath = NULL;
+   yinit.mpegpath = NULL;
+   yinit.cartpath = NULL;
+   yinit.frameskip = 0;
+   yinit.videoformattype = VIDEOFORMATTYPE_NTSC;
+   yinit.clocksync = 0;
+   yinit.basetime = 0;
+   yinit.skip_load = 1;
 
-    if (YabauseInit(&yinit) != 0)
-        return -1;
+   if (YabauseInit(&yinit) != 0)
+      return -1;
 
-    MappedMemoryLoadExec(argv[1], 0);
+   MappedMemoryLoadExec(argv[1], 0);
 
-    MappedMemoryWriteByte(VDP2_VRAM + AUTO_TEST_SELECT_ADDRESS, current_test);
+   MappedMemoryWriteByte(VDP2_VRAM + AUTO_TEST_SELECT_ADDRESS, current_test);
 
-    for (;;)
-    {
-        int status = 0;
+   for (;;)
+   {
+      int status = 0;
 
-        PERCore->HandleEvents();
+      PERCore->HandleEvents();
 
-        status = MappedMemoryReadByte(VDP2_VRAM + AUTO_TEST_STATUS_ADDRESS);
+      status = MappedMemoryReadByte(VDP2_VRAM + AUTO_TEST_STATUS_ADDRESS);
 
-        if (status != AUTO_TEST_NOT_RUNNING &&
-            status != AUTO_TEST_BUSY)
-        {
-            int i = 0;
+      if (status != AUTO_TEST_NOT_RUNNING &&
+         status != AUTO_TEST_BUSY)
+      {
+         int i = 0;
 
-            for (;;)
+         for (;;)
+         {
+            char test_info[256] = { 0 };
+            char command[64] = { 0 };
+            char fail_command[64] = { 0 };
+
+            get_test_strings(&i, &test_info[0], &command[0]);
+            strncpy(fail_command, command, 4);
+
+            if (!strcmp(command, "MESSAGE"))
             {
-                char test_info[256] = { 0 };
-                char command[64] = { 0 };
-                char fail_command[64] = { 0 };
-
-                get_test_strings(&i, &test_info[0], &command[0]);
-                strncpy(fail_command, command, 4);
-
-                if (!strcmp(command, "MESSAGE"))
-                {
-                    //write a message without doing anything else
-                    printf("%-32s\n", test_info);
-                }
-                else if (!strcmp(command, "PASS"))
-                {
-                    //sub-test passed, continue to next
-                    print_result(&test_info[0], &command[0], TEST_PASSED);
-                    tests_total++;
-                    tests_passed++;
-                    continue;
-                }
-                else if (!strcmp(fail_command, "FAIL"))
-                {
-                    //sub-test failure, due to pre-existing
-                    //emulation issues, not a regression
-                    int found = 0;
-                    int j = 0;
-                    for (j = 0; tests_expected_to_fail[j] != NULL; j++)
-                    {
-                        if (!strcmp(test_info, tests_expected_to_fail[j]))
-                        {
-                            print_result(&test_info[0], &command[0], TEST_FAILED_EXPECTED);
-                            tests_total++;
-                            expected_failure_count++;
-                            found = 1;
-                            break;
-                        }
-                    }
-
-                    if (found)
-                    {
-                        continue;
-                    }
-
-                    //sub-test failure, regression
-                    print_result(&test_info[0], &command[0], TEST_FAILED);
-                    tests_total++;
-                    regressions++;
-                    continue;
-                }
-                else if (!strcmp(command, "NEXT"))
-                {
-                    //all sub-tests finished, proceed to next main test
-                    printf("\n");
-                    current_test++;
-
-                    YabauseDeInit();
-
-                    if (YabauseInit(&yinit) != 0)
-                        return -1;
-
-                    MappedMemoryLoadExec(argv[1], 0);
-                    MappedMemoryWriteByte(VDP2_VRAM + AUTO_TEST_SELECT_ADDRESS, current_test);
-                    break;
-
-                }
-                else if (!strcmp(command, "QUIT"))
-                {
-                    //all tests completed, exit
-
-                    if (regressions > 0)
-                    {
-                        set_color(text_red);
-                    }
-                    else
-                    {
-                        set_color(text_green);
-                    }
-
-                    printf("%d of %d tests passed. %d regressions. %d failures that are not regressions. \n", tests_passed, tests_total, regressions, expected_failure_count);
-
-                    set_color(text_white);
-
-                    finished = 1;
-                    break;
-                }
+               //write a message without doing anything else
+               printf("%-32s\n", test_info);
             }
-
-            if (finished)
+            else if (!strcmp(command, "PASS"))
             {
-                break;
+               //sub-test passed, continue to next
+               print_result(&test_info[0], &command[0], TEST_PASSED);
+               tests_total++;
+               tests_passed++;
+               continue;
             }
-        }
-    }
+            else if (!strcmp(fail_command, "FAIL"))
+            {
+               //sub-test failure, due to pre-existing
+               //emulation issues, not a regression
+               int found = 0;
+               int j = 0;
+               for (j = 0; tests_expected_to_fail[j] != NULL; j++)
+               {
+                  if (!strcmp(test_info, tests_expected_to_fail[j]))
+                  {
+                     print_result(&test_info[0], &command[0], TEST_FAILED_EXPECTED);
+                     tests_total++;
+                     expected_failure_count++;
+                     found = 1;
+                     break;
+                  }
+               }
 
-    return regressions;
+               if (found)
+               {
+                  continue;
+               }
+
+               //sub-test failure, regression
+               print_result(&test_info[0], &command[0], TEST_FAILED);
+               tests_total++;
+               regressions++;
+               continue;
+            }
+            else if (!strcmp(command, "NEXT"))
+            {
+               //all sub-tests finished, proceed to next main test
+               printf("\n");
+               current_test++;
+
+               YabauseDeInit();
+
+               if (YabauseInit(&yinit) != 0)
+                  return -1;
+
+               MappedMemoryLoadExec(argv[1], 0);
+               MappedMemoryWriteByte(VDP2_VRAM + AUTO_TEST_SELECT_ADDRESS, current_test);
+               break;
+
+            }
+            else if (!strcmp(command, "QUIT"))
+            {
+               //all tests completed, exit
+
+               if (regressions > 0)
+               {
+                  set_color(text_red);
+               }
+               else
+               {
+                  set_color(text_green);
+               }
+
+               printf("%d of %d tests passed. %d regressions. %d failures that are not regressions. \n", tests_passed, tests_total, regressions, expected_failure_count);
+
+               set_color(text_white);
+
+               finished = 1;
+               break;
+            }
+         }
+
+         if (finished)
+         {
+            break;
+         }
+      }
+   }
+
+   return regressions;
 }
